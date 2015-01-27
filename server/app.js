903e80d68fd5d36ced62c84d577c01dbd2eae8e8
Jason <git@jasonpoon.ca>
2015-01-27 00:28:44 -0800

    serve ember app from server

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
