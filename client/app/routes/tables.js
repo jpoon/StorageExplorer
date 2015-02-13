bcc5353f4c9635093c2298c8962115bd96598a4a
Jason <git@jasonpoon.ca>
2015-02-13 02:19:38 -0800

    add error model checking

--- client/app/routes/tables.js
+++ client/app/routes/tables.js
@@ -14,21 +14,16 @@ export default Ember.Route.extend({
     },
 
     model: function() {
-        var that = this;
         var storageAccountName = this.controllerFor("application").get('storageAccountName');
         var storageAccountKey = this.controllerFor("application").get('storageAccountKey');
 
         var url = config.APP.apiHost + '/tables?account=' + storageAccountName + '&key=' + storageAccountKey;
         Ember.Logger.info('url', url);
         
-        var data = Ember.$.getJSON(url);
-
-        Ember.run.later(function(){
-            that.controllerFor("application").set('showProgress', false);
-            data.abort();
-        }, 5000);
-
-        return data;
+        return Ember.$.getJSON(url)
+                    .fail(function(error) {
+                        throw new Error(error);
+                    });
     },
 
     afterModel: function() {
@@ -37,11 +32,8 @@ export default Ember.Route.extend({
 
     actions: {
         error: function(error) {
-            console.log(error);
-            if (error && error.status === 400) {
-                // error substate and parent routes do not handle this error
-                return this.transitionTo('modelNotFound');
-            }
+            Ember.Logger.error(error);
+            this.controllerFor("application").set('showProgress', false);
         }
     }
 });
