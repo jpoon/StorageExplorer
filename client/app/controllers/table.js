31b4427e1526034f344930f94c0feef01ca0187b
Jason <git@jasonpoon.ca>
2015-04-23 10:22:10 -0700

    add row template/controller. to do: move sorting from table -> row

--- client/app/controllers/table.js
+++ client/app/controllers/table.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  sortProperty: 'partitionKey',
-  sortAscending: 'true',
-
   glyphiconDirection: function() {
     if (this.get('sortAscending')) {
        return "glyphicon-chevron-down";
@@ -13,17 +10,18 @@ export default Ember.ObjectController.extend({
   }.property("sortAscending"),
 
   sortedRows: function(){
-    var array = this.get('content.rows').sortBy(this.get('sortProperty'));
+    var array = this.get('model.rows').sortBy(this.get('sortProperty'));
+
     if (this.get('sortAscending')) {
       array.reverse();
     }
 
     return array;
-  }.property('content.rows', 'sortProperty', 'sortAscending'),  
+  }.property('model.rows', 'sortProperty', 'sortAscending'),
 
   rowCount: function(){
-    return this.get('content.rows').length;
-  }.property('content.rows'),
+    return this.get('model.rows').length;
+  }.property('model.rows'),
 
   actions: {
     sortBy: function(property) {
--- client/app/models/row.js
+++ client/app/models/row.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
 
 var row = DS.Model.extend({
+    table: DS.belongsTo('table'),
     partitionKey: DS.attr('string'),
     rowKey: DS.attr('string'),
     timestamp: DS.attr('string'),
-    table: DS.belongsTo('table'),
     columns: DS.hasMany('column', { embedded: 'always'})
 });
 
