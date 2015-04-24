273a229d9b42e9f3fd99825de0c0150e95e3f495
Jason <git@jasonpoon.ca>
2015-04-24 00:07:03 -0700

    UI fixes. Align button to right. Add font-awesome

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -3,16 +3,21 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-	vendorFiles: {
-		'handlebars.js': null
-	}
+	vendorFiles: { 'handlebars.js': null },
+    emberCliFontAwesome: { includeFontAwesomeAssets: false }
 });
 
 app.import('bower_components/bootswatch-dist/js/bootstrap.min.js');
 app.import('bower_components/bootswatch-dist/css/bootstrap.min.css');
-app.import('bower_components/bootswatch-dist/fonts/glyphicons-halflings-regular.woff', {
-  destDir: 'fonts'
-});
+app.import('bower_components/bootswatch-dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
+
+app.import("bower_components/font-awesome/css/font-awesome.css");
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
