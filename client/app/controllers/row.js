759e78271e8afe024ca10a2b50326d071b112d81
Jason <git@jasonpoon.ca>
2015-04-23 21:29:03 -0700

    move sorting to row class

--- client/app/controllers/row.js
+++ client/app/controllers/row.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  sortProperties: ['partitionKey'],
-  sortAscending: false,
+    needs: ['table'],
+
+    sortProperties: ['partitionKey'],
+    sortAscending: true,
+
+    sortPropertiesComputed: function() {
+        this.set('sortProperties', [this.get('controllers.table.sortProperty')]);
+    }.observes('controllers.table.sortProperty'),
+
+    sortAscendingComputed: function() {
+        this.set('sortAscending', this.get('controllers.table.sortAscending'));
+    }.observes('controllers.table.sortAscending'),
 });
--- client/app/controllers/table.js
+++ client/app/controllers/table.js
@@ -1,32 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  glyphiconDirection: function() {
-    if (this.get('sortAscending')) {
-       return "glyphicon-chevron-down";
-    } else {
-       return "glyphicon-chevron-up";
+    sortProperty: '',
+    sortAscending: true,
+
+    glyphiconDirection: function() {
+      if (this.get('sortAscending')) {
+         return "glyphicon-chevron-down";
+      } else {
+         return "glyphicon-chevron-up";
+      }
+    }.property("sortAscending"),
+
+    rowCount: function(){
+      return this.get('model.rows').length;
+    }.property('model.rows'),
+
+    actions: {
+      sortBy: function(property) {
+        this.set('sortProperty', property);
+        this.set('sortAscending', !this.get('sortAscending'));
+      }
     }
-  }.property("sortAscending"),
-
-  sortedRows: function(){
-    var array = this.get('model.rows').sortBy(this.get('sortProperty'));
-
-    if (this.get('sortAscending')) {
-      array.reverse();
-    }
-
-    return array;
-  }.property('model.rows', 'sortProperty', 'sortAscending'),
-
-  rowCount: function(){
-    return this.get('model.rows').length;
-  }.property('model.rows'),
-
-  actions: {
-    sortBy: function(property) {
-      this.set('sortProperty', property);
-      this.set('sortAscending', !this.get('sortAscending'));
-    }
-  }
 });
