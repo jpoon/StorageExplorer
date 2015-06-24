d32747f0d97decc27f6da166c3c7f1813eef3494
Jason <git@jasonpoon.ca>
2015-06-24 01:21:47 -0700

    build production. fix warnings

--- client/app/helpers/is-column-sortable.js
+++ client/app/helpers/is-column-sortable.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(value, options) {
-  return value == 0 || value == 1;
+export default Ember.Handlebars.makeBoundHelper(function(value) {
+  return value === 0 || value === 1;
 });
