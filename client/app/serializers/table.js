beb38ca3dbc709d449ee93c7e3efa428453cb284
Jason <git@jasonpoon.ca>
2015-06-24 01:15:29 -0700

    display table name

--- client/app/serializers/table.js
+++ client/app/serializers/table.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
-    primaryKey: 'tableName',
+  primaryKey: 'tableName',
 
 	attrs: {
 		rows: { embedded: 'always' }
