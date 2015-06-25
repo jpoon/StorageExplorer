bb1d4d1f938fd54728091c4b4eed6ca920a9ace1
Jason <git@jasonpoon.ca>
2015-06-24 23:54:39 -0700

    don't leak stacktraces on prod

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-var debug = require('debug')('app')
-    , express = require('express')
-    , path = require('path')
-    , favicon = require('serve-favicon')
-    , logger = require('morgan')
-    , cookieParser = require('cookie-parser')
-    , bodyParser = require('body-parser')
-    , cors = require('cors');
+var debug = require('debug')('app'),
+    express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
 var routes = require('./routes/index');
 var tables = require('./routes/tables');
@@ -18,15 +18,17 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.disable('etag');
@@ -35,38 +37,36 @@ app.use('/', routes);
 app.use('/tables', tables);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    debug('404');
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  debug('404');
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        debug(err.message);
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+  // development error handler
+  // will print stacktrace
+  app.use(function(err, req, res, next) {
     debug(err.message);
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: err
     });
-});
-
+  });
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    debug(err.message);
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+}
 
 module.exports = app;
